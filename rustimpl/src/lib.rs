624d127556d167eb26ca09ab343473372ce4a877
John Aughey <jha@aughey.com>
2023-08-24 20:38:48 +0000

    additional rust work

--- rustimpl/src/lib.rs
+++ rustimpl/src/lib.rs
@@ -1,96 +1,104 @@
 #![no_std]
 
-pub fn split_string_3(value: &str, delimiter: &str) -> Option<(u16,u16,u16)> {
+pub fn split_string_try_op(value: &str, delimiter: &str) -> Option<(u16, u16, u16)> {
+    let first_pos = value.find(delimiter)?;
+    let first_substr = value.get(0..first_pos)?;
+    let first = first_substr.parse::<i32>().ok()?;
+    if first < 0 || first > 65535 {
+        return None;
+    }
+    let first_remaining = value.get((first_pos + delimiter.len())..)?;
+
+    let second_pos = first_remaining.find(delimiter)?;
+    let second_substr = first_remaining.get(0..second_pos)?;
+    let second = second_substr.parse::<i32>().ok()?;
+    if second < 0 || second > 65535 {
+        return None;
+    }
+    let second_remaining = first_remaining.get((second_pos + delimiter.len())..)?;
+
+    let third = second_remaining.parse::<i32>().ok()?;
+    if third < 0 || third > 65535 {
+        return None;
+    }
+    // Range checking was done above, this static cast is valid
+    return Some((first as u16, second as u16, third as u16));
+}
+
+pub fn split_string_like_cpp(value: &str, delimiter: &str) -> Option<(u16, u16, u16)> {
+    let first_pos = value.find(delimiter);
+    if first_pos.is_none() {
+        return None;
+    }
+    let first_pos_index = first_pos.unwrap();
+
+    let first_substr = value.get(0..first_pos_index).unwrap(); // "safe" because of first_pos.is_some()
+    let first_conversation = first_substr.parse::<i32>();
+    if first_conversation.is_err() {
+        return None;
+    }
+    let first = first_conversation.unwrap();
+    if first < 0 || first > 65535 {
+        return None;
+    }
+    // Get a substring that is the remaining of the string with the first and the delimiter removed
+    let first_remaining = value.get((first_pos_index + delimiter.len())..).unwrap(); // "safe" because of first_pos.is_some()
+
+    let second_pos = first_remaining.find(delimiter);
+    if second_pos.is_none() {
+        return None;
+    }
+    let second_pos_index = second_pos.unwrap();
+
+    let second_substr = first_remaining.get(0..second_pos_index).unwrap(); // "safe" because of second_pos.is_some()
+    let second_conversation = second_substr.parse::<i32>();
+    if second_conversation.is_err() {
+        return None;
+    }
+    let second = second_conversation.unwrap();
+    if second < 0 || second > 65535 {
+        return None;
+    }
+    // Get a substring that is the remaining of the string with the second and the delimiter removed
+    let second_remaining = first_remaining
+        .get((second_pos_index + delimiter.len())..)
+        .unwrap(); // "safe" because of second_pos.is_some()
+
+    // Third is just a parse of the second remaining
+    let third_conversation = second_remaining.parse::<i32>();
+    if third_conversation.is_err() {
+        return None;
+    }
+    let third = third_conversation.unwrap();
+    if third < 0 || third > 65535 {
+        return None;
+    }
+
+    // Range checking was done above, this static cast is valid
+    return Some((first as u16, second as u16, third as u16));
+}
+
+pub fn split_string_iterator_combinator(value: &str, delimiter: &str) -> Option<(u16, u16, u16)> {
+    let mut parts = value.split(delimiter).map(|v| v.parse::<u16>().ok());
+
+    Some((
+        parts.next()??, // Double ??, first ? unwraps the next, second ? unwraps the parse
+        parts.next()??,
+        parts.next().and_then(|v| parts.next().xor(Some(v)))??,
+    ))
+}
+
+pub fn split_string_3(value: &str, delimiter: &str) -> Option<(u16, u16, u16)> {
     let mut parts = value.split(delimiter);
-    let hours = parts.next()?.parse::<u16>().ok()?;
-    let minutes = parts.next()?.parse::<u16>().ok()?;
-    let seconds = parts.next()?.parse::<u16>().ok()?;
+
+    let first = parts.next()?.parse::<u16>().ok()?;
+    let second = parts.next()?.parse::<u16>().ok()?;
+    let third = parts.next()?.parse::<u16>().ok()?;
+
     // should be no more in parts
     if parts.next().is_some() {
         return None;
     }
-    Some((hours, minutes, seconds))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn happy_path() {
-        let res = split_string_3("1,2,3", ",");
-        assert_eq!(res, Some((1,2,3)));
-    }
-
-    #[test]
-    fn missing_one() {
-        let res = split_string_3("1,2",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn hole() {
-        let res = split_string_3("1,,3",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn not_number() {
-        let res = split_string_3("A,2,3",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn out_of_range_3() {
-        let res = split_string_3("1,2,65536",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn out_of_range_2() {
-        let res = split_string_3("1,65536,3",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn out_of_range_1() {
-        let res = split_string_3("65536,2,3",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn low_range_pass() {
-        let res = split_string_3("0,0,0",",");
-        assert_eq!(res, Some((0,0,0)));
-    }
-
-    #[test]
-    fn low_range_fail_1() {
-        let res = split_string_3("-1,0,0",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn low_range_fail_2() {
-        let res = split_string_3("0,-1,0",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn low_range_fail_3() {
-        let res = split_string_3("0,0,-1",",");
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn high_range_pass() {
-        let res = split_string_3("65535,65535,65535",",");
-        assert_eq!(res, Some((65535,65535,65535)));
-    }
-
-    #[test]
-    fn too_many_numbers() {
-        let res = split_string_3("1,2,3,4",",");
-        assert_eq!(res, None);
-    }
+    
+    Some((first, second, third))
 }
---
+++ rustimpl/tests/string_tests.rs
@@ -0,0 +1,89 @@
+use rstest::rstest;
+use rstest_reuse::{self, *};
+use rustimpl::*;
+
+#[template]
+#[rstest]
+#[case(split_string_try_op)]
+#[case(split_string_3)]
+#[case(split_string_like_cpp)]
+#[case(split_string_iterator_combinator)]
+fn all_splits(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {}
+
+#[apply(all_splits)]
+fn happy_path(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("1,2,3", ",");
+    assert_eq!(res, Some((1, 2, 3)));
+}
+
+#[apply(all_splits)]
+fn missing_one(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("1,2", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn hole(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("1,,3", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn not_number(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("A,2,3", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn out_of_range_3(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("1,2,65536", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn out_of_range_2(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("1,65536,3", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn out_of_range_1(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("65536,2,3", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn low_range_pass(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("0,0,0", ",");
+    assert_eq!(res, Some((0, 0, 0)));
+}
+
+#[apply(all_splits)]
+fn low_range_fail_1(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("-1,0,0", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn low_range_fail_2(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("0,-1,0", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn low_range_fail_3(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("0,0,-1", ",");
+    assert_eq!(res, None);
+}
+
+#[apply(all_splits)]
+fn high_range_pass(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("65535,65535,65535", ",");
+    assert_eq!(res, Some((65535, 65535, 65535)));
+}
+
+#[apply(all_splits)]
+fn too_many_numbers(#[case] split : impl Fn(&str, &str) -> Option<(u16, u16, u16)>) {
+    let res = split("1,2,3,4", ",");
+    assert_eq!(res, None);
+}
